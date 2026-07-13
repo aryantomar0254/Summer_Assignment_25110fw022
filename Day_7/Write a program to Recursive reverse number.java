bf1c3980c113ae054bfcdf66d48522d704143d90
aryantomar0254 <aryantomar0254@gmail.com>
2026-07-13 07:55:24 +0530

    Create Write a program to Recursive reverse number.java

---
+++ Day_7/Write a program to Recursive reverse number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+static int rev=0;
+static int reverse(int n){
+if(n==0)
+return rev;
+rev=rev*10+n%10;
+return reverse(n/10);
+}
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+System.out.println(reverse(n));
+}
+}
