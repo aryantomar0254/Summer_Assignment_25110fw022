5bee4b3c660338ef2aa7e6cccb73d12efec40f0d
aryantomar0254 <aryantomar0254@gmail.com>
2026-07-15 00:10:09 +0530

    Create Write a program to Write function to find sum  of two numbers.java

---
+++ Day_11/Write a program to Write function to find sum  of two numbers.java
@@ -0,0 +1,11 @@
+public class Main{
+    static int sum(int a,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args){
+        int num1=10;
+        int num2=20;
+        System.out.println("Sum = "+sum(num1,num2));
+    }
+}
